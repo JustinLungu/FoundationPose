0f43dc7c6630c896a21b65e648efe68db83ec4ba
JustinLungu <iustinl113@gmail.com>
2024-09-03 11:33:23 +0200

    bind mount for dev changes

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+
+# remove any existing container "foundationpose"
 docker rm -f foundationpose
-DIR=$(pwd)/../
-xhost +  && docker run --gpus all --env NVIDIA_DISABLE_REQUIRE=1 -it --network=host --name foundationpose  --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -v $DIR:$DIR -v /home:/home -v /mnt:/mnt -v /tmp/.X11-unix:/tmp/.X11-unix -v /tmp:/tmp  --ipc=host -e DISPLAY=${DISPLAY} -e GIT_INDEX_FILE foundationpose:latest bash -c "cd $DIR && bash"
+
+# set the project directory (assumed to be the parent of the 'docker' directory)
+PROJECT_DIR=$(dirname $(dirname $(realpath $0)))
+
+# run the Docker container
+xhost +  # for container GUI
+docker run --gpus all \
+  --env NVIDIA_DISABLE_REQUIRE=1 \
+  -it \
+  --network=host \
+  --name foundationpose \
+  --cap-add=SYS_PTRACE \
+  --security-opt seccomp=unconfined \
+  -v $PROJECT_DIR:/app \
+  -v /home:/home \
+  -v /mnt:/mnt \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /tmp:/tmp \
+  --ipc=host \
+  -e DISPLAY=${DISPLAY} \
+  -e GIT_INDEX_FILE \
+  foundationpose:latest \
+  bash -c "cd /app && bash"
